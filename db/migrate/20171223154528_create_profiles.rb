7fae4e8f19b8bcea1a346008856d092c8e835316
Morita Jun <morita.jn0124@gmail.com>
2017-12-24 00:46:17 +0900

    add model profile

---
+++ db/migrate/20171223154528_create_profiles.rb
@@ -0,0 +1,12 @@
+class CreateProfiles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :profiles do |t|
+      t.string :name
+      t.date :birth
+      t.intger :sex
+      t.string :school_name
+
+      t.timestamps
+    end
+  end
+end
