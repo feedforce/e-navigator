5e8bb04ca91ab3e017d57c52b81dd005e3690b28
Morita Jun <morita.jn0124@gmail.com>
2017-12-24 00:48:59 +0900

    add model Profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223153151) do
+ActiveRecord::Schema.define(version: 20171223154528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.date "birth"
+    t.integer "sex"
+    t.string "school_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
