0c1b40b0063dbeb83458ff3362272695f12ff6fe
Morita Jun <morita.jn0124@gmail.com>
2017-12-24 14:00:57 +0900

    add validate birthday

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   enum sex: { man: 1, woman: 2 }
+  
+  validate :birthday_cannot_be_in_the_future
+
+  def birthday_cannot_be_in_the_future
+    if birthday.present? && birthday > Date.today
+      errors.add(:birthday, "can not specify your future date as your birth date.")
+    end
+  end
+
 end
