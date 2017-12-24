e9a8831544fedb7a4e47e1b002cde2edef786a87
Morita Jun <morita.jn0124@gmail.com>
2017-12-24 13:00:15 +0900

    add profile strong parameter for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  DEVISE_PERMISSIBLE_ATTRIBUTES = %i(username birth sex schoolname)
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: DEVISE_PERMISSIBLE_ATTRIBUTES)
+    devise_parameter_sanitizer.permit(:account_update, keys: DEVISE_PERMISSIBLE_ATTRIBUTES)
+  end
 end
