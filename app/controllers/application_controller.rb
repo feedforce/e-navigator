1877f8ec80693f746df1bd38a57e4eec6b89ea64
Morita Jun <morita.jn0124@gmail.com>
2017-12-24 13:28:19 +0900

    bug fixsd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  DEVISE_PERMISSIBLE_ATTRIBUTES = %i(username birth sex schoolname)
+  DEVISE_PERMISSIBLE_ATTRIBUTES = %i(username birthday sex schoolname)
 
   protected
 
