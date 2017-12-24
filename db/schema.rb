92372ca65e3859e7224f5c39794d5a83484035cf
Morita Jun <morita.jn0124@gmail.com>
2017-12-24 12:52:41 +0900

    Add profile items to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223154528) do
+ActiveRecord::Schema.define(version: 20171224034548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20171223154528) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.date "birthday"
+    t.integer "sex"
+    t.string "schoolname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
