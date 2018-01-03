f0b2710085480bfb07f7d7c3ad2509fc21c325d0
daichi <anaminkumamoto@gmail.com>
2018-01-03 20:14:46 +0900

    add_first_change

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/' => 'home#top'
+
+  get 'user/new' => 'user#new'
+  post 'user/create' => 'user#create'
+  post 'user/:id/edit' => 'user#edit'
+  post 'user/:id/update' => 'user#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180103104720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "birthday"
+    t.string "sex"
+    t.string "school_name"
+  end
 
 end
