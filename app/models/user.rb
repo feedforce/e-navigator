84f4109fc368fb39ca24e296ebf241b04713f1de
cobafan <akitaka0405@gmail.com>
2018-01-07 01:56:33 +0900

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  enum gender: { unknown: 0, male: 1, female: 2}
+end
