83be41345196f4dd2bb0d6dc97ad99f98c670130
chionyan <ariariarissa@gmail.com>
2018-01-11 02:42:40 +0900

    前半修正一通り完了しました

---
+++ app/controllers/toppages_controller.rb
@@ -0,0 +1,7 @@
+class ToppagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+   }
+
+  root to: 'toppages#index'
+
+  get 'toppages/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
