05e12b1d6e131f511bc5a5dfde38c138be71370d
Mitsuwo <hapowalp@gmail.com>
2018-01-13 20:04:02 +0900

    ユーザ登録、ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top/index'
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords     => "users/passwords"
+  }
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
+
+  root 'top#index'
 end
