d5f564588140d81ab9da6b3748832fa6d2b89c1d
R-Msb <ryu@MBP.local>
2018-01-19 02:13:45 +0900

    add profile update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,16 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      redirect_to edit_user_path(id: current_user.id)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :birthday, :sex, :school)
   end
 end
