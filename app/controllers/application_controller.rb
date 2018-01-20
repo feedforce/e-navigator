f28c9c28f42142a0245706549d081bc31bb08bb3
AK <ak-mac@AK-MBP.local>
2018-01-20 21:01:11 +0900

    ログインページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+      edit_user_path(current_user.id)
+  end
+  #日程調整機能実装作業開始時に削除
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  #日程調整機能実装作業開始時に修正
 end
