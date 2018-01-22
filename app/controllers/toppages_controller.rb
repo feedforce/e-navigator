3170036bccfc2241be09db7a94a3ba412865fe17
chionyan <ariariarissa@gmail.com>
2018-01-22 23:37:11 +0900

    UserController作成,ルート変更

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,4 @@
 class ToppagesController < ApplicationController
   def index
   end
-
-  def show
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   root 'toppages#index'
 
-  get 'toppages/show'
+  get 'users/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
