17f89cfa2a7d7dc434d75011763b6b180a5802d6
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-01-22 09:53:10 +0000

    userモデルのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  validates :school, presence: true
+  
+  
+  
 end
