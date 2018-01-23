8aec96f2c9146d18a512cd96a02697f5058d65b6
R-Msb <ryu@MBP.local>
2018-01-23 19:45:45 +0900

    add : create interview action and some links
    面接作成機能の実装、各ページへのリンクを配置

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,5 +1,26 @@
 class InterviewsController < ApplicationController
   def index
-    @interviews = current_user.interviews.all
+    @interviews = current_user.interviews.order("interview_date ASC")
+  end
+
+  def new
+    @user = current_user
+    @interview = Interview.new
+  end
+
+  def create
+    @user = current_user
+    @interview = Interview.new(interview_params)
+    if @interview.save
+      redirect_to user_interviews_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def interview_params
+    params.require(:interview).permit(:interview_date).merge(user_id: params[:user_id])
   end
 end
