e17f0ad900d6e706781e450c10cdb4cc80d13ccb
masahiro tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-01-23 12:16:31 +0000

    add security_filter

--- db/migrate/20180122090920_create_users.rb
+++ db/migrate/20180122090920_create_users.rb
@@ -6,7 +6,12 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :password_digest
       t.date :birth_of_date
       t.string :school
-
+      
+      # OAuth認証 用のカラム
+      t.string :provider
+      t.string :uid
+      t.string :nickname
+      
       t.timestamps
     end
   end
