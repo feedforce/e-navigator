4ec33b3e6831df6d1f38708eb4d4675a181eba66
R-Msb <ryu@MBP.local>
2018-01-23 17:25:58 +0900

    fix @user var
    userモデルの定義方法を修正
    共通機能になりそうなのでメソッドとして定義し、beforeフィルターを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   def index
   end
 
   def edit
-    @user = User.find(current_user.id)
   end
 
   def update
-    @user = User.find(current_user.id)
     if @user.update(user_params)
       redirect_to users_path
     else
-      redirect_to edit_user_path(id: current_user.id)
+      render edit_user_path(current_user)
     end
   end
 
   private
   def user_params
     params.require(:user).permit(:email, :name, :birthday, :sex, :school)
+  end
+
+  def set_user
+    @user = current_user
   end
 end
