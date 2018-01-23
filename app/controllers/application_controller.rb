64a9f23208fb8bbe2a1b421849efb94d4b700cc2
h-hiroki <gyouzakuuka@gmail.com>
2018-01-23 19:40:18 +0900

    Fix routing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, unless: :devise_controller?
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do
-    authenticated :user do
-      root 'users#index', as: :authenticated_root
-    end
-    unauthenticated :user do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
+  root 'users#index'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
