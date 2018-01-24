c175d44b583e70f0a4b2dacc0f82c22288478692
R-Msb <ryu@MBP.local>
2018-01-24 23:37:02 +0900

    add validation for interview date

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,8 @@
 class Interview < ApplicationRecord
   belongs_to :user
+  validate :check_datetime
+
+  def check_datetime
+    errors[:base] << "日時は現在以降を設定してください。" if self.interview_date < Time.current
+  end
 end
