241db451a48657b81d1a4ffe71697cd3edf1bb03
R-Msb <ryu@MBP.local>
2018-01-24 12:17:32 +0900

    add before_aciton : set_user, set_interview
    共通アクションをメソッド定義、beforeフィルターで実行

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,16 +1,16 @@
 class InterviewsController < ApplicationController
+  before_action :set_user, except: :destroy
+  before_action :set_interview, only: [:edit, :update, :destroy]
+
   def index
-    @user = current_user
     @interviews = @user.interviews.order("interview_date ASC")
   end
 
   def new
-    @user = current_user
     @interview = Interview.new
   end
 
   def create
-    @user = current_user
     @interview = Interview.new(interview_params)
     if @interview.save
       redirect_to user_interviews_url
@@ -20,13 +20,9 @@ class InterviewsController < ApplicationController
   end
 
   def edit
-    @user = current_user
-    @interview = Interview.find(params[:id])
   end
 
   def update
-    @user = current_user
-    @interview = Interview.find(params[:id])
     if  @interview.update(interview_params)
       redirect_to user_interviews_url
     else
@@ -35,13 +31,19 @@ class InterviewsController < ApplicationController
   end
 
   def destroy
-    @user = current_user
-    @interview = Interview.find(params[:id])
     @interview.destroy
     redirect_to user_interviews_url
   end
 
   private
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_interview
+    @interview = Interview.find(params[:id])
+  end
 
   def interview_params
     params.require(:interview).permit(:interview_date).merge(user_id: params[:user_id])
