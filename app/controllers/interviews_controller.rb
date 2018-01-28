43227c53499ad67e1076196f447ddf87a4ab6659
h-hiroki <gyouzakuuka@gmail.com>
2018-01-28 22:15:22 +0900

    Add interviews edit and destroy function.

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -14,12 +14,37 @@ class InterviewsController < ApplicationController
     if @interview.save
       redirect_to user_interviews_path, notice: '日程を作成しました'
     else
-      redirect_to user_interviews_path, notice: '失敗しました。やり直して下さい'
+      redirect_to user_interviews_path, alert: '失敗しました。やり直して下さい'
+    end
+  end
+
+  def edit
+    @interview = Interview.find(params[:id])
+  end
+
+  def update
+    if Interview.update(params[:id], update_params)
+      redirect_to user_interviews_path, notice: '日程情報を修正しました'
+    else
+      redirect_to user_interviews_path, alert: '失敗しました。やり直して下さい'
+    end
+  end
+
+  def destroy
+    interview = Interview.find(params[:id])
+    if interview.destroy
+      redirect_to user_interviews_path, notice: '日程を削除しました'
+    else
+      redirect_to user_interviews_path, alert: '失敗しました。やり直して下さい'
     end
   end
 
   private
   def create_params
     params.require(:interview).permit(:interview_datetime).merge(user_id: current_user.id)
+  end
+
+  def update_params
+    params.require(:interview).permit(:interview_datetime)
   end
 end
