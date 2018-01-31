bcec32b2f2f2f55aec2b0677bd6a2957782dd0ac
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2018-01-31 17:29:56 +0900

    Set user_age_method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum gender: { 男性: 0, 女性: 1 }
+
+  def age
+    (Date.today.strftime("%Y%m%d").to_i - birth_date.strftime("%Y%m%d").to_i) / 10000
+  end
 end
