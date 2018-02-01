bb8db1eb7a879459b5a8253bf4a0a5c2c57b8e89
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2018-02-01 11:24:59 +0900

    Modify application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
