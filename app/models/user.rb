bd1764f23b5e51bb3b6123966c83de60dd89fc00
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2018-02-01 12:15:52 +0900

    Modify user.model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   enum gender: { 男性: 0, 女性: 1 }
 
   def age
+    if birth_date.present?
     (Date.today.strftime("%Y%m%d").to_i - birth_date.strftime("%Y%m%d").to_i) / 10000
+    else
+      return ""
+    end
   end
 end
