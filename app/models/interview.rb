180ba9ffc235a576e9af72fd0620b0c05b31d48a
kai815 <h.kaizuka815@gmail.com>
2018-02-05 19:38:31 +0900

    Set interview validation

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,11 @@
 class Interview < ApplicationRecord
   belongs_to :user
   enum status: { default: 0, approved: 1, declined: 2 }
+  validate :datetime_avalable
+
+  def datetime_avalable
+    if start_time.present? && start_time < 1.days.from_now
+      errors.add(:start_time, "は今日の日付よりも1日以上後の日付を入力してください")
+    end
+  end
 end
