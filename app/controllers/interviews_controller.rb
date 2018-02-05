2dcb829c8a0657a33d7a682dd1e5a042c46bd9aa
kai815 <h.kaizuka815@gmail.com>
2018-02-05 19:35:47 +0900

    Set interview_controller action

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,2 +1,24 @@
 class InterviewsController < ApplicationController
+  def index
+    @interviews = current_user.interviews.order(" start_time ASC")
+  end
+
+  def new
+    @interview = Interview.new
+  end
+
+  def create
+    @interview = Interview.new(interview_params)
+    if @interview.save
+      redirect_to root_path, notice: "面接日程を追加しました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def interview_params
+    params.require(:interview).permit(:start_time).merge(user_id: params[:user_id])
+  end
 end
