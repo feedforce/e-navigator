7d82ab28554673e3a534fd401814643c0c6729ef
R-Msb <>
2018-02-13 22:49:18 +0900

    update other_interviews-index
    現在設定されている面談の有無で表示内容を分岐するように設定
    helperにメソッドを定義しviewから呼び出し

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -1,2 +1,10 @@
 module InterviewsHelper
+
+  def get_current_interview(user)
+    Interview.find_by(user_id: user.id, status: 1)
+  end
+
+  def check_current_interview(interview)
+    interview ? interview.interview_date.strftime("%Y年%m月%d日 %H時%M分") + "に面談が設定されています" : "現在面談は設定されていません"
+  end
 end
