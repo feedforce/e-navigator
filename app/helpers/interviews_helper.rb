e78651cda698e4aece3b098e64984bc420c982b0
R-Msb <>
2018-02-14 17:31:33 +0900

    fix date_format method
    引数が存在しない場合に対応できるように修正

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -9,6 +9,6 @@ module InterviewsHelper
   end
 
   def date_format(interview)
-    return interview.interview_date.strftime("%Y年%m月%d日 %H時%M分")
+    return interview.interview_date.strftime("%Y年%m月%d日 %H時%M分") if interview
   end
 end
