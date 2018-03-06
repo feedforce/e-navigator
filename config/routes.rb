9c3c8f728a181b39bb81dbc6ba003e0d5700e26a
EC2 Default User <ec2-user@ip-10-0-1-10.ap-northeast-1.compute.internal>
2018-03-07 00:01:11 +0900

    add to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   get 'pages/show'
 
+  as :user do
+    get 'devise/registrations#edit', :to => 'pages#show', :as => :user_root
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
