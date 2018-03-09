75c9de15bf63280f63478f247c83b0ef18d2ca80
稲員未来 <miku.inakazu@gmail.com>
2018-03-09 22:01:26 +0900

    add instance in order to newly make profile and save it to the profile table

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,15 +4,29 @@ class ProfilesController < ApplicationController
   end
 
   def new
+    @profile = Profile.new
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to :root, notice: 'Your profile is successfully saved'
+    else
+      redirect_to :root, notice: 'Unsuccessfully saved'
+    end
   end
 
   def edit
+    @profile = Profile.edit
   end
 
   def update
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:email, :name, :birth_year, :birth_month, :birth_day, :sex, :school)
   end
 
 end
