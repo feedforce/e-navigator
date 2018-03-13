0e4c5a69271f1b563e54048cf132561884fd8372
稲員未来 <miku.inakazu@gmail.com>
2018-03-13 11:59:54 +0900

    modify profiles table

--- db/migrate/20180309083045_create_profiles.rb
+++ db/migrate/20180309083045_create_profiles.rb
@@ -4,7 +4,7 @@ class CreateProfiles < ActiveRecord::Migration[5.1]
       t.string  :email
       t.string  :name
       t.integer :birth_year
-      t.integer :birth_month
+      t.string  :birth_month
       t.integer :birth_day
       t.string  :sex
       t.string  :school
