300fcb497c14d547de1740c9b08866e12047dedf
s_akiba <bbcap.megane@gmail.com>
2018-03-23 10:37:57 +0900

    routes の設定変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/show'
 
-  get 'pages/index'
-
-  get 'pages/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
