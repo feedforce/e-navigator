7e7c9d19019ec133272268ee44c73b6209a870f7
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-03 03:29:16 +0900

    Create user profile and edit pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :authenticate_user!
+	def after_sign_in_path_for(resource)
+		user_path(@user)
+	end
+
+	def after_sign_out_path_for(resource)
+		root_path
+	end
+
+	def configure_permitted_parameters
+		added_attrs = [ :school, :name, :email, :password, :password_confirmation, :sex, :birthday　]
+		devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+		devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+		devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+	end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+	def index
+		@users = User.where.not(id: params[:id]).order(created_at: 'desc')
+	end
+
+	def show
+		@user = User.find_by(id: params[:id])
+	end
+end
