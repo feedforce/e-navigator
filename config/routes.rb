496ff215959d35d155395cb1947d379a821774e9
EikiKanaoka <kakakaeee@gmail.com>
2018-04-05 18:53:58 +0900

    devise setting && users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :users, only:[:index, :edit, :update]
+  root to: 'users#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
