2e82986abb1250143dfe8ebcecdaceabf15473b7
EikiKanaoka <kakakaeee@gmail.com>
2018-04-09 18:37:07 +0900

    interviews controller&view 作成

---
+++ app/controllers/interviews_controller.rb
@@ -0,0 +1,30 @@
+class InterviewsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def interview_params
+  end
+
+  def correct_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+
   devise_for :users, only: [:sign_in, :sign_out, :session, :registerations]
   as :user do
     get '/users/sign_up', to: 'devise/registrations#new', as: :new_user_registration
     post '/users', to: 'devise/registrations#create', as: :user_registration
   end
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:index, :edit, :update] do
+    resources :interviews
+  end
+
   root to: 'users#index'
 end
