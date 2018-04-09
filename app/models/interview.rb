b8e534f7359cfe9483adf7e62a875fcda3a0cfad
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-10 04:44:20 +0900

    Add enum setting to app/models/interview.rb

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -3,4 +3,9 @@ class Interview < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :datetime, presence: true
+  enum status: {
+    suspended: 0,
+    approved: 1,
+    declined: 2
+  }
 end
