c60f5028b55ab11a086e5c019fc66902abea0d0c
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-10 04:49:39 +0900

    Add routing for Interview

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     root "users/sessions#new"
   end
-  resources :users, only: [:index, :show]
+
+  resources :users, only: [:index, :show] do
+    resources :interviews
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
