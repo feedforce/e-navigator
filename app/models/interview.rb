0062de300b2d024f1cf096e277434225cd62d0d0
EikiKanaoka <kakakaeee@gmail.com>
2018-04-09 18:30:29 +0900

    interviewsモデル作成

---
+++ app/models/interview.rb
@@ -0,0 +1,5 @@
+class Interview < ApplicationRecord
+  belongs_to :user
+  
+  enum availability: { hold: 0, approval: 1, rejected: 2 }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+  has_many :interviews
   # 性別の設定
   enum gender: { male: 0, female: 1 }
 
