9776464b841c5f08c4455c2b70083f87b37f4569
EikiKanaoka <kakakaeee@gmail.com>
2018-04-09 19:54:47 +0900

    interview controller view 一覧 作成 表示

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,14 +1,27 @@
 class InterviewsController < ApplicationController
+
   def index
+    @user = User.find(params[:user_id])
+    @interviews = @user.interviews.all
   end
 
   def show
+    @interview = current_user.interviews.find(params[:id])
   end
 
   def new
+    @interview = current_user.interviews.build
   end
 
   def create
+    @interview = current_user.interviews.build(interview_params)
+    if Time.now < @interview.schedule
+      flash[:danger] = '未来の時間を指定してください'
+      redirect_to new_user_interview_path
+    elsif @interview.save
+      flash[:info] = "面接日程を作成しました"
+      redirect_to user_interview_path(id: @interview)
+    end
   end
 
   def edit
@@ -20,11 +33,21 @@ class InterviewsController < ApplicationController
   def destroy
   end
 
+  def time_now(time)
+    if Time.now < time
+      flash[:danger] = '未来の時間を指定してください'
+      render :new
+    end
+  end
+
+
   private
 
   def interview_params
+    params.require(:interview).permit(:schedule)
   end
 
   def correct_user
   end
+
 end
