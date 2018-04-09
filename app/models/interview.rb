cb7b007186714e18d23d67d4fcd830cba6cb96b3
EikiKanaoka <kakakaeee@gmail.com>
2018-04-09 18:48:41 +0900

    rails db:migrate

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,5 +1,4 @@
 class Interview < ApplicationRecord
   belongs_to :user
-  
   enum availability: { hold: 0, approval: 1, rejected: 2 }
 end
--- db/migrate/20180409092720_create_interviews.rb
+++ db/migrate/20180409092720_create_interviews.rb
@@ -2,7 +2,7 @@ class CreateInterviews < ActiveRecord::Migration[5.1]
   def change
     create_table :interviews do |t|
       t.datetime :schedule
-      t.interger :availability, default: 0, null: false, limit: 1
+      t.integer :availability, default: 0, null: false, limit: 1
       t.references :user, foreign_key: true
 
       t.timestamps
