b752310b871f1dfd0211ec9874c65d8a35725552
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-12 06:58:31 +0900

    Set current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  before_action :set_corrent_user
+  before_action :current_user
 
   def configure_permitted_parameters
     added_attrs = [ :name, :email, :sex, :birthday, :school, :　]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
-  end
-  
-  def set_corrent_user
-    current_user ||= User.find_by(id: session[:user_id])
   end
 end
