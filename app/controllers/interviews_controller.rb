b877e376a8882524f2e3ff089d75b4663a5c9dd9
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-12 08:41:28 +0900

    Integrate user searching as get_user

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,19 +1,17 @@
 class InterviewsController < ApplicationController
+  before_action :get_user, only: [:index, :new, :create, :edit, :update, :destroy]
   before_action :ensure_correct_user_to_create, only: [:new, :create]
   before_action :ensure_correct_user_to_edit_and_delete, only: [:edit, :update, :destroy]
 
   def index
-    @user = User.find_by(id: params[:user_id])
     @interviews = @user.interviews
   end
 
   def new
-    @user = User.find_by(id: params[:user_id])
     @interview = Interview.new
   end
 
   def create
-    @user = User.find_by(id: params[:user_id])
     @interview = @user.interviews.new(interview_params)
     if @interview.save
       flash[:success] = "面接日時を登録しました"
@@ -24,12 +22,10 @@ class InterviewsController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(id: params[:user_id])
     @interview = @user.interviews.find_by(id: params[:id])
   end
 
   def update
-    @user = User.find_by(id: params[:user_id])
     @interview = @user.interviews.find_by(id: params[:id])
     if @interview.update(interview_params)
       redirect_to user_interviews_path
@@ -39,14 +35,12 @@ class InterviewsController < ApplicationController
   end
 
   def destroy
-    @user = User.find_by(id: params[:user_id])
     @interview = @user.interviews.find_by(id: params[:id])
     @interview.destroy
     redirect_to user_interviews_path
   end
 
 	def ensure_correct_user_to_create
-    @user = User.find_by(id: params[:user_id])
 		if @user.id != current_user.id
 			flash[:notice] = "他のユーザーの面接日時の登録は許可されていません"
 			redirect_to users_path
@@ -60,6 +54,10 @@ class InterviewsController < ApplicationController
 			redirect_to users_path
 		end
 	end
+
+  def get_user
+    @user = User.find_by(id: params[:user_id])
+  end
 
   private
 
