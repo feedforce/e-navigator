2cb89c5c5ecd7382cf3fc29bbde890089fbe8e73
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-12 07:45:49 +0900

    Add confirmation message in loggin out

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,5 +1,6 @@
 class InterviewsController < ApplicationController
-  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :ensure_correct_user_to_create, only: [:new, :create]
+  before_action :ensure_correct_user_to_edit_and_delete, only: [:edit, :update, :destroy]
 
   def index
     @user = User.find_by(id: params[:user_id])
@@ -44,7 +45,15 @@ class InterviewsController < ApplicationController
     redirect_to user_interviews_path
   end
 
-	def ensure_correct_user
+	def ensure_correct_user_to_create
+    @user = User.find_by(id: params[:user_id])
+		if @user.id != current_user.id
+			flash[:notice] = "他のユーザーの面接日時の登録は許可されていません"
+			redirect_to users_path
+		end
+	end
+
+	def ensure_correct_user_to_edit_and_delete
 		@interview = Interview.find_by(id: params[:id])
 		if @interview.user_id != current_user.id
 			flash[:notice] = "他のユーザーの面接日時の編集及び削除は許可されていません"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,6 @@ module ApplicationHelper
     :user
   end
 
-  def resource
-    @resource ||= User.new
-  end
-
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
