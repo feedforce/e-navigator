af80c8219bcc71c7f77dab8e1eed34ebc072aa31
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-14 11:24:41 +0900

    Modify edit and update, callback function, indent setting

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,5 +1,6 @@
 class InterviewsController < ApplicationController
-  before_action :get_user, only: [:index, :new, :create, :edit, :update, :destroy]
+  before_action :set_user, only: [:index, :new, :create]
+  before_action :set_interview, only: [:edit, :update, :destroy]
   before_action :ensure_correct_user_to_create, only: [:new, :create]
   before_action :ensure_correct_user_to_edit_and_delete, only: [:edit, :update, :destroy]
 
@@ -22,11 +23,9 @@ class InterviewsController < ApplicationController
   end
 
   def edit
-    @interview = @user.interviews.find_by(id: params[:id])
   end
 
   def update
-    @interview = @user.interviews.find_by(id: params[:id])
     if @interview.update(interview_params)
       redirect_to user_interviews_path
     else
@@ -35,28 +34,30 @@ class InterviewsController < ApplicationController
   end
 
   def destroy
-    @interview = @user.interviews.find_by(id: params[:id])
     @interview.destroy
     redirect_to user_interviews_path
   end
 
-	def ensure_correct_user_to_create
-		if @user.id != current_user.id
-			flash[:notice] = "他のユーザーの面接日時の登録は許可されていません"
-			redirect_to users_path
-		end
-	end
+  def ensure_correct_user_to_create
+    if @user.id != current_user.id
+      flash[:notice] = "他のユーザーの面接日時の登録は許可されていません"
+      redirect_to users_path
+    end
+  end
 
-	def ensure_correct_user_to_edit_and_delete
-		@interview = Interview.find_by(id: params[:id])
-		if @interview.user_id != current_user.id
-			flash[:notice] = "他のユーザーの面接日時の編集及び削除は許可されていません"
-			redirect_to users_path
-		end
-	end
+  def ensure_correct_user_to_edit_and_delete
+    if @interview.user_id != current_user.id
+      flash[:notice] = "他のユーザーの面接日時の編集及び削除は許可されていません"
+      redirect_to users_path
+    end
+  end
 
-  def get_user
+  def set_user
     @user = User.find_by(id: params[:user_id])
+  end
+
+  def set_interview
+    @interview = Interview.find_by(id: params[:id])
   end
 
   private
