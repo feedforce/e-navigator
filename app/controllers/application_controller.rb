cc8b4d54cf3967a1dd6a49fa114fe08ac085c569
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-14 11:22:47 +0900

    Remove unnecessary before_action :current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  before_action :current_user
 
   def configure_permitted_parameters
     added_attrs = [ :name, :email, :sex, :birthday, :school, :　]
