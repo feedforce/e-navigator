424afb37a7e87bb53b5d90fe756e298641689b98
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-16 08:57:39 +0900

    Add comment to each method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
+  # GET /users
   def index
-    @users = User.where.not(id: current_user.id)
+    @users = User.where.not(id: current_user.id).order(id: 'asc')
   end
 
+  # GET /users/:id
   def show
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
   end
 end
