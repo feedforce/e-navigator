83d49d1dc22fb5356c97986f124140b48f47e098
EikiKanaoka <kakakaeee@gmail.com>
2018-04-16 06:42:43 +0900

    ageメソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   # 年齢の判別
   def age
     date_format = "%Y%m%d"
-    (Date.today.strftime(date_format).to_i - self.birthdate.strftime(date_format).to_i) / 10000
+    (Time.current.strftime(date_format).to_i - self.birthdate.strftime(date_format).to_i) / 10000
   end
 
 end
