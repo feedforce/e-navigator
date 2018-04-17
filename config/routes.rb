7eace92972edc0af0a6c4d4645b481382e0668ee
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-17 09:08:23 +0900

    Add routing for approve action

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,12 @@ Rails.application.routes.draw do
   devise_scope :user do
     root "users/sessions#new"
   end
-
   resources :users, only: [:index, :show] do
-    resources :interviews, except: [:show]
+    resources :interviews, except: [:show] do
+      member do
+        post 'approve'
+      end
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
