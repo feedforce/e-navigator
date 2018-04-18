c3a264b27a00857925b61dd3c1ac7e243bdf2efc
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-18 10:10:43 +0900

    Add @approved_interview, enum helper method and modify callback

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,12 +1,11 @@
 class InterviewsController < ApplicationController
   before_action :set_user, only: [:index, :new, :create]
   before_action :set_interview, only: [:edit, :update, :destroy, :approve]
-  before_action :ensure_correct_user_to_create, only: [:new, :create]
-  before_action :ensure_correct_user_to_update_and_delete, only: [:edit, :update, :destroy]
 
   # GET /users/:user_id/interviews
   def index
     @interviews = @user.interviews.order(datetime: 'asc')
+    @approved_interview = @interviews.find_by(status: 1)
   end
 
   # GET /users/:user_id/interviews/new
@@ -16,12 +15,17 @@ class InterviewsController < ApplicationController
 
   # POST /users/:user_id/interviews
   def create
-    @interview = @user.interviews.new(interview_params)
-    if @interview.save
-      flash[:success] = "面接日時を登録しました"
-      redirect_to user_interviews_path
+    unless current_user?(@user.id)
+      flash[:notice] = "他のユーザーの面接日時の登録は許可されていません"
+      redirect_to users_path
     else
-      render :new
+      @interview = @user.interviews.new(interview_params)
+      if @interview.save
+        flash[:notice] = "面接日時を登録しました"
+        redirect_to user_interviews_path
+      else
+        render :new
+      end
     end
   end
 
@@ -31,55 +35,50 @@ class InterviewsController < ApplicationController
 
   # PATCH /users/:user_id/interviews/:id
   def update
-    @interview.update(interview_params)
-    flash[:success] = "面接日時を変更しました"
-    redirect_to user_interviews_path
+    unless current_user?(@interview.user.id)
+      flash[:notice] = "他のユーザーの面接日時の変更は許可されていません"
+      redirect_to users_path
+    else
+      @interview.update(interview_params)
+      flash[:notice] = "面接日時を変更しました"
+      redirect_to user_interviews_path
+    end
   end
 
   # DELETE /users/:user_id/interviews/:id
   def destroy
     @interview.destroy
-    flash[:success] = "面接日時を削除しました"
+    flash[:notice] = "面接日時を削除しました"
     redirect_to user_interviews_path
   end
 
   # POST /users/:user_id/interviews/:id/approve
   def approve
-    @interview.update(status: 1)
+    @interview.approved!
     # 以下、承認した日時以外を否認状態にする
     interviews = Interview.where(user_id: @interview.user_id).where.not(id: @interview.id)
     interviews.each do |interview|
-      interview.update(status: 2)
+      interview.declined!
     end
-    flash[:success] = "面接日時を承認しました"
+    flash[:notice] = "面接日時を承認しました"
     redirect_to user_interviews_path
-  end
-
-  def ensure_correct_user_to_create
-    if @user.id != current_user.id
-      flash[:notice] = "他のユーザーの面接日時の登録は許可されていません"
-      redirect_to users_path
-    end
-  end
-
-  def set_user
-    @user = User.find_by(id: params[:user_id])
-  end
-
-  def set_interview
-    @interview = Interview.find(params[:id])
-  end
-
-  def ensure_correct_user_to_update_and_delete
-    if @interview.user_id != current_user.id
-      flash[:notice] = "他のユーザーの面接日時の編集及び削除は許可されていません"
-      redirect_to users_path
-    end
   end
 
   private
 
+    def set_user
+      @user = User.find_by(id: params[:user_id])
+    end
+
+    def set_interview
+      @interview = Interview.find(params[:id])
+    end
+
     def interview_params
       params.require(:interview).permit(:datetime)
+    end
+
+    def current_user?(id)
+      id == current_user
     end
 end
