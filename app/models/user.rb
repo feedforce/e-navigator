1cc81c648ac054a5c8627b0a9d6cda4054fbd6b6
EikiKanaoka <kakakaeee@gmail.com>
2018-04-20 19:54:47 +0900

    ageの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
          :rememberable, :validatable
   has_many :interviews
   enum gender: { male: 0, female: 1 }
-  
   # 年齢の判別
   def age
-    (Time.current.try(:strftime, "%Y%m%d").to_i - self.birthdate.strftime( "%Y%m%d").to_i) / 10000
+    (Time.current.strftime("%Y%m%d").to_i - self.birthdate.try(:strftime, "%Y%m%d").to_i) / 10000
   end
 end
