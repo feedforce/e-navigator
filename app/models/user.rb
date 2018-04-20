c7616956e39b9a20c9cda52d66a84048af934bdf
EikiKanaoka <kakakaeee@gmail.com>
2018-04-20 19:04:16 +0900

    style & models/user.rb（ageの修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
   has_many :interviews
-  # 性別の設定
   enum gender: { male: 0, female: 1 }
-
+  
   # 年齢の判別
   def age
-    (Time.current.try(:strftime, "%Y%m%d").to_i - self.birthdate.try(:strftime, "%Y%m%d").to_i) / 10000
+    (Time.current.try(:strftime, "%Y%m%d").to_i - self.birthdate.strftime( "%Y%m%d").to_i) / 10000
   end
-
 end
