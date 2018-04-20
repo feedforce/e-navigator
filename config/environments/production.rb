1c4be1caad5ad89ab690ec4be3236c38be5565c5
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-20 11:26:25 +0900

    Modify host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # ActionMailer
-  config.action_mailer.default_url_options = { host: 'example.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'e-navigator-hayato-ishida.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: ENV['MAILER_USER_ID'],
