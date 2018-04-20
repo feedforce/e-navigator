f5d2468f78f2d76f3528875652d1ef1ddb246593
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-21 05:02:39 +0900

    Add user and approver as argument to select_approver

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,9 @@
 class NotificationMailer < ApplicationMailer
   helper ApplicationHelper
 
-  def notification_new_interview(interview)
-    @interview = interview
-    users = User.where.not(id: @interview.user_id)
-    users.each do |user|
-    mail(to: user.email, subject: "新規面接日時が登録されました")
-    end
+  def notification_new_interview(user, approver)
+    @user = user
+    mail(to: approver.email, subject: "新規面接日時が登録されました")
   end
 
   def notification_approved_interview(approver, interview)
