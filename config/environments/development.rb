1ef739c442bafedc24ba0af21665396103896e4c
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-21 10:45:59 +0900

    Add https as protocol

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # ActionMailer
-	config.action_mailer.default_url_options = { host: '192.168.33.10', port: 3000 }
+	config.action_mailer.default_url_options = { host: '192.168.33.10', port: 3000, protocol: 'https' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
