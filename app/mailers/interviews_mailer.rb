53ad5b89f327d0d76d33e37a885baaa2cb8bf261
Hayato-Ishida <bin.lhkty.gtthj.o.zs@gmail.com>
2018-04-23 11:49:25 +0900

    Modify ActionMailer method name

---
+++ app/mailers/interviews_mailer.rb
@@ -0,0 +1,16 @@
+class InterviewsMailer < ApplicationMailer
+  helper ApplicationHelper
+
+  # メソッド名を変更しました
+  def apply(user, approver)
+    @user = user
+    mail(to: approver.email, subject: "新規面接日時が登録されました")
+  end
+
+  # メソッド名を変更しました
+  def approve(approver, interview)
+    @interview = interview
+    @approver = approver
+    mail(to: interview.user.email, cc: approver.email, subject: "面接日時が確定しました")
+  end
+end
