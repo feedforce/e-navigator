5dab2ec911accb233780f5406647aac1166c5535
froover <dio22038@gmail.com>
2018-06-26 19:55:27 +0900

    レイアウト調整、プロフィール情報の保存

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,10 @@
 // about supported directives.
 //
 
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require rails-ujs
 //= require popper
 //= require bootstrap-sprockets
-
+//= require_tree .
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     pages_show_path
   end
@@ -9,5 +11,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:age])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:school])
   end
 end
