8490ece4fe987a392902b4d7359b6da70f3cbbf3
吉野輝 <shoynoi.jp@gmail.com>
2018-10-06 21:12:37 +0900

    ユーザー情報を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum gender: { male: 0, female: 1 }
+  enum gender: { man: 0, woman: 1 }
+
+  def age
+    date_format = "%Y%m%d"
+    (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
+  end
 end
