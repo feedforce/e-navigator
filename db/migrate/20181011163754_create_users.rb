a46f854bc6f040caa6f29331ab660d3993eede57
EC2 Default User <ec2-user@ip-172-31-24-140.us-east-2.compute.internal>
2018-10-14 19:01:07 +0000

    complete first step

--- db/migrate/20181011163754_create_users.rb
+++ db/migrate/20181011163754_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :gender
+      t.integer :gender
       t.string :school
 
       t.timestamps
--- db/migrate/20181014174013_change_datatype_gender_of_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeGenderOfUsers < ActiveRecord::Migration[5.1]
-  def change
-    change_column :users, :gender, :integer
-  end
-end
