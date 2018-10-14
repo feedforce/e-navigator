70f9e66d0205f6a180462a83bb6d4564cf865c9f
EC2 Default User <ec2-user@ip-172-31-24-140.us-east-2.compute.internal>
2018-10-14 18:18:18 +0000

    Complete first step

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil:true
-  enum gender: { Male: 0, Female: 1 ,Secret:2}
+  enum gender: { Male: 0, Female: 1 }
   validates :school, length: { maximum: 50 }
 end
