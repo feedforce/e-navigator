cbe27011e0e9f236ab961e907af726c3a5431cba
EC2 Default User <ec2-user@ip-172-31-24-140.us-east-2.compute.internal>
2018-10-15 12:24:30 +0000

    bug fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update, :show]
   
   def show
     @user = User.find(params[:id])
