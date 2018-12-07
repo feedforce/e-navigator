1fe8ad841449bc6cbac63986a6a7507f97d9e1bc
takaykuki0310 <t.k.y079c@gmail.com>
2018-12-08 08:04:48 +0900

    authenticate pages show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: :show
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/index'
 
+  root to: "home#index"
+  get 'pages/index'
   get 'pages/show'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
