9318ad3b051472596221334ae2006cfdf74b398e
Takuya KIkuchi <takuyakikuchi@TakuyanoMacBook-Air.local>
2019-02-26 13:31:35 +0900

    migrate

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  # mailer setting
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
