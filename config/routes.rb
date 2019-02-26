3054cba72b51a0a760b4a52ce0620baf0852466a
yamadakj <47668294+yamadakj@users.noreply.github.com>
2019-02-26 12:15:45 +0900

    Revert "Test"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'homes/show'
-
-  devise_for :users
-  root to: "homes#show"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20190224020159_add_columns_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :gender, :integer
-    add_column :users, :birthdate, :date
-    add_column :users, :school, :string
-  end
-end
