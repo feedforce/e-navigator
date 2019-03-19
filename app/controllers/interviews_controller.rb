1971bc360b938925d8eb7f4e628962970ec78ffe
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-03-19 19:05:21 +0900

    ログインしているユーザーの面接日程のみを登録、編集、削除が行えるように変更

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,5 +1,5 @@
 class InterviewsController < ApplicationController
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:edit,:update,:destroy]
 
   def index
     @user = User.find(params[:user_id])
@@ -12,31 +12,24 @@ class InterviewsController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:user_id])
-    @interview = @user.interviews.build
+    @interview = current_user.interviews.build
   end
 
   def create
-    user = User.find(params[:user_id])
-    @interview = user.interviews.build(interview_params)
+    @interview = current_user.interviews.build(interview_params)
     @interview.save!
-    redirect_to user_interview_path(user_id: user.id, id: @interview.id), notice: "面接が作成されました。"
+    redirect_to user_interview_path(user_id: current_user.id, id: @interview.id), notice: "面接が作成されました。"
   end
 
   def edit
-    @user = User.find(params[:user_id])
-    @interview = @user.interviews.find(params[:id])
   end
 
   def update
-    user = User.find(params[:user_id])
-    interview = user.interviews.find(params[:id])
-    interview.update!(interview_params)
-    redirect_to user_interview_path(user_id: user.id, id: interview.id), notice: "面接が更新されました。"
+    @interview.update!(interview_params)
+    redirect_to user_interview_path(user_id: current_user.id, id: @interview.id), notice: "面接が更新されました。"
   end
 
   def destroy
-    #interview = Interview.find(params[:id])
     @interview.destroy
     redirect_to user_interviews_path(user_id: @interview.user.id), notice: "面接日程を削除しました。"
   end
@@ -49,7 +42,7 @@ class InterviewsController < ApplicationController
 
   def correct_user
     @interview = current_user.interviews.find_by(id: params[:id])
-    unless @interview
+    unless @interview.present?
       redirect_to root_url
     end
   end
