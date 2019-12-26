9a2ace854696df4c8baadec1878403e16fa0ea99
punkshiraishi <pharmacy.sy@gmail.com>
2019-12-27 00:37:22 +0900

    Generate pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class PagesController < ApplicationController
+  def index; end
+
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
