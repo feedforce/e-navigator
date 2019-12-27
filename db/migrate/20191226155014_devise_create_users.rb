c59205fd6793a91f3e989ce12916d5674d5a95c1
punkshiraishi <pharmacy.sy@gmail.com>
2019-12-27 12:09:41 +0900

    Migrate device user

--- db/migrate/20191226155014_devise_create_users.rb
+++ db/migrate/20191226155014_devise_create_users.rb
@@ -4,12 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ''
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
-
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -35,8 +35,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :email, unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
