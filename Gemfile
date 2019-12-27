0683be3a20638c1871622fd2467832fe4af72334
punkshiraishi <pharmacy.sy@gmail.com>
2019-12-27 12:25:39 +0900

    Install Japanese patch for device

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
+gem 'devise-i18n'
+gem 'devise-i18n-views'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module ENavigator
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
