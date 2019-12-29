eaa63747ce5a9d5c31435c186d1348d0d96d9b87
punkshiraishi <pharmacy.sy@gmail.com>
2019-12-29 19:59:42 +0900

    Arrange routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  def home
+    redirect_to login_path unless user_signed_in?
+  end
+
   def index; end
 
   def show; end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  root 'pages#home'
   get 'pages/index'
   get 'pages/show'
 
