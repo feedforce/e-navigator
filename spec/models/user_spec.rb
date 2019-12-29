f5ed65da16a92f5f0103793e86b26caa7744e725
punkshiraishi <pharmacy.sy@gmail.com>
2019-12-30 07:49:40 +0900

    Add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,27 @@ RSpec.describe User, type: :model do
     it 'has a valid factory' do
       expect(FactoryBot.build(:user)).to be_valid
     end
+
+    it 'is valid with email and password' do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
+
+    it 'is invalid without email' do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include('を入力してください')
+    end
+
+    it 'is invalid without password' do
+      user = FactoryBot.build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include('を入力してください')
+    end
+
+    it 'is invalid without short password' do
+      user = FactoryBot.build(:user, password: 'a' * 5)
+      user.valid?
+      expect(user.errors[:password]).to include('は6文字以上で入力してください')
+    end
   end
 end
