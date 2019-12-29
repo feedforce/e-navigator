d0445550f622d4900521cfe2496fad9870b86911
punkshiraishi <pharmacy.sy@gmail.com>
2019-12-29 15:45:17 +0900

    Create header

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/index'
   get 'pages/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    post 'login', to: 'devise/sessions#create'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
 end
