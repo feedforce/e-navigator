204882d160a779d9d3fdcf3058d08bac4c21a511
punkshiraishi <pharmacy.sy@gmail.com>
2019-12-29 22:12:26 +0900

    Adjust rspec settings

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe User do
+    it 'has a valid factory' do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
+  end
 end
