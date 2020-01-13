f7b8575f90eab398dbd2e8575a1582083e5bb2af
punkshiraishi <pharmacy.sy@gmail.com>
2020-01-13 18:27:40 +0900

    Restore bin/** file
    自動生成されるファイルのうちrubocopで自動修正したものを元の状態に戻した。masterにmergeするときに差分のあるファイルを無駄に増やさないために行った。

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,20 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_229_031_210) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name', default: '', null: false
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.date 'birth'
-    t.integer 'gender'
-    t.string 'school'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
 end
