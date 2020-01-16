0083da0a2fe6c69bb816753db160584fcd27568a
punkshiraishi <pharmacy.sy@gmail.com>
2020-01-16 22:57:10 +0900

    bundle install to update rack
    Rackバージョンアップによると思われる不具合があったため再インストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_12_31_060823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interviews", force: :cascade do |t|
+    t.datetime "schedule"
+    t.boolean "attendable"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "interviewer_id"
+    t.integer "interviewee_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.date "birth"
+    t.integer "gender"
+    t.string "school"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
