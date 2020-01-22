b4ff856331145fbdd428342fc30dd34dce0797f8
punkshiraishi <pharmacy.sy@gmail.com>
2020-01-22 20:50:46 +0900

    $rails db:migrate
    マイグレーションファイル修正後のマイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_31_060823) do
+ActiveRecord::Schema.define(version: 2019_12_26_155014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "interviews", force: :cascade do |t|
-    t.datetime "schedule"
-    t.boolean "attendable"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "interviewer_id"
-    t.integer "interviewee_id"
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "school"
+    t.datetime "birth"
+    t.integer "gender"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.date "birth"
-    t.integer "gender"
-    t.string "school"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
