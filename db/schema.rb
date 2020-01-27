5131d0d0e63bd5320c6f4bc7d0fcaa3e62ce27b5
punkshiraishi <pharmacy.sy@gmail.com>
2020-01-27 19:31:28 +0900

    $ rails db:migrate$
    interviews テーブルをDBに登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_155014) do
+ActiveRecord::Schema.define(version: 2020_01_27_101651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interviews", force: :cascade do |t|
+    t.datetime "schedule"
+    t.boolean "attendable"
+    t.integer "interviewer_id"
+    t.integer "interviewee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
