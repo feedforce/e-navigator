0d068783511b999ab4a2825df174eb07ad6f91cc
punkshiraishi <pharmacy.sy@gmail.com>
2020-01-30 12:30:26 +0900

    $ rails db:migrate
    interviewモデルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_101651) do
+ActiveRecord::Schema.define(version: 2020_01_30_032708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2020_01_27_101651) do
   create_table "interviews", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "interviewer_id"
+    t.integer "interviewee_id"
+    t.datetime "scheduled_at"
   end
 
   create_table "users", force: :cascade do |t|
