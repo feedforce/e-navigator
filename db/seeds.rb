632cade6919724aac66212e3fbe4bbe595bf6c2e
punkshiraishi <pharmacy.sy@gmail.com>
2020-01-31 01:11:40 +0900

    Add user seeds
    サンプルとなるユーザ情報を簡単に登録できるようにシードを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: 'Yuma Shiraishi',
+             email: 'shiraishi@test.com',
+             password: 'password',
+             password_confirmation: 'password')
+
+User.create!(name: 'Yosuke Obata',
+             email: 'obata@test.com',
+             password: 'password',
+             password_confirmation: 'password')
+
+User.create!(name: 'Miki Miyazato',
+             email: 'miyazato@test.com',
+             password: 'password',
+             password_confirmation: 'password')
