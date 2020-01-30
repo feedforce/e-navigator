fdd0356715ee2976429e1845c843eb98170307d4
punkshiraishi <pharmacy.sy@gmail.com>
2020-01-31 01:20:40 +0900

    Add validates to interview
    interviewモデルの面接者、面接官、予定日を入力必須にした

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,4 +2,7 @@
 
 class Interview < ApplicationRecord
   belongs_to :interviewer, class_name: 'User'
+  validates :interviewer_id, presence: true
+  validates :interviewee_id, presence: true
+  validates :scheduled_at, presence: true
 end
