965ba53001fd2ee6d4eb2c76cbf8e986c3537abc
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-03 21:30:20 +0900

    Clean user spec indent
    無駄にネストしてたので修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,31 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe User do
-    it 'has a valid factory' do
-      expect(FactoryBot.build(:user)).to be_valid
-    end
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
 
-    it 'is valid with email and password' do
-      expect(FactoryBot.build(:user)).to be_valid
-    end
+  it 'is valid with email and password' do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
 
-    it 'is invalid without email' do
-      user = FactoryBot.build(:user, email: nil)
-      user.valid?
-      expect(user.errors[:email]).to include(I18n.t('errors.messages.blank'))
-    end
+  it 'is invalid without email' do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include(I18n.t('errors.messages.blank'))
+  end
 
-    it 'is invalid without password' do
-      user = FactoryBot.build(:user, password: nil)
-      user.valid?
-      expect(user.errors[:password]).to include(I18n.t('errors.messages.blank'))
-    end
+  it 'is invalid without password' do
+    user = FactoryBot.build(:user, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include(I18n.t('errors.messages.blank'))
+  end
 
-    it 'is invalid with a short password' do
-      user = FactoryBot.build(:user, password: 'a' * 5)
-      user.valid?
-      expect(user.errors[:password]).to include(I18n.t('errors.messages.too_short', count: 6))
-    end
+  it 'is invalid with a short password' do
+    user = FactoryBot.build(:user, password: 'a' * 5)
+    user.valid?
+    expect(user.errors[:password]).to include(I18n.t('errors.messages.too_short', count: 6))
   end
 end
