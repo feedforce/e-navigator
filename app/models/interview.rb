55fbda45b3a2c1d77850ef1364c9f8c1827d2a1e
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-04 23:11:04 +0900

    Change model to belong to user simply

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Interview < ApplicationRecord
-  belongs_to :interviewer, class_name: 'User'
+  belongs_to :user
   validates :interviewer_id, presence: true
   validates :interviewee_id, presence: true
   validates :scheduled_at, presence: true
