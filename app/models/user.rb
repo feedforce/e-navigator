59f5a52066e70a8d21be63c452e7a9aff48abc62
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-04 22:54:35 +0900

    Delete foreign key of user
    userが持っているinterviewモデルの外部キーを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :interviews, foreign_key: 'interviewee_id'
+  has_many :interviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
