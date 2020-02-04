e5fe08bbd5c2339fe40e522fa38cf5a7d190deb1
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-04 23:14:54 +0900

    Delete interviewee_id validation
    今後interviewee_idのカラムを廃止するためバリデーションも削除

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -3,6 +3,5 @@
 class Interview < ApplicationRecord
   belongs_to :user
   validates :interviewer_id, presence: true
-  validates :interviewee_id, presence: true
   validates :scheduled_at, presence: true
 end
