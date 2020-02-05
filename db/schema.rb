f73e49269f418403b4102a1897e769766aa11037
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-06 00:33:35 +0900

    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_161612) do
+ActiveRecord::Schema.define(version: 2020_02_05_150400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_161612) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 1
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
