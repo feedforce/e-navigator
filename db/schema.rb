a0503788e76b0b39d44ed0c5c1c77f5f9124a48d
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-05 20:54:57 +0900

    $rails db:migrate
    interviewのinterviewee_idをuser_idに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_032708) do
+ActiveRecord::Schema.define(version: 2020_02_04_161612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_01_30_032708) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "interviewer_id"
-    t.integer "interviewee_id"
+    t.integer "user_id"
     t.datetime "scheduled_at"
   end
 
