985c1869328f370d69c701f342414d1517aea816
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-07 23:22:48 +0900

    Add rubocop styles
    引数や配列やハッシュの最後の値の後につくカンマはそのままでOKに変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,17 @@
 User.create!(name: 'Yuma Shiraishi',
              email: 'shiraishi@test.com',
              password: 'password',
-             password_confirmation: 'password')
+             assword_confirmation: 'password',
+            )
 
 User.create!(name: 'Yosuke Obata',
              email: 'obata@test.com',
              password: 'password',
-             password_confirmation: 'password')
+             password_confirmation: 'password',
+            )
 
 User.create!(name: 'Miki Miyazato',
              email: 'miyazato@test.com',
              password: 'password',
-             password_confirmation: 'password')
+             password_confirmation: 'password',
+            )
