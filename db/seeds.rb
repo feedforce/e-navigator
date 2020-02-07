b315bccf28b37437f055259ba940610a952617ad
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-08 00:06:44 +0900

    Apply Style/TrailingCommaInArguments
    ルボコップの自動整形で複数行の配列、ハッシュ、引数の最後の値にカンマを付与

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
-User.create!(name: 'Yuma Shiraishi',
-             email: 'shiraishi@test.com',
-             password: 'password',
-             assword_confirmation: 'password',
-            )
+User.create!(
+  name: 'Yuma Shiraishi',
+  email: 'shiraishi@test.com',
+  password: 'password',
+  assword_confirmation: 'password',
+)
 
-User.create!(name: 'Yosuke Obata',
-             email: 'obata@test.com',
-             password: 'password',
-             password_confirmation: 'password',
-            )
+User.create!(
+  name: 'Yosuke Obata',
+  email: 'obata@test.com',
+  password: 'password',
+  password_confirmation: 'password',
+)
 
-User.create!(name: 'Miki Miyazato',
-             email: 'miyazato@test.com',
-             password: 'password',
-             password_confirmation: 'password',
-            )
+User.create!(
+  name: 'Miki Miyazato',
+  email: 'miyazato@test.com',
+  password: 'password',
+  password_confirmation: 'password',
+)
