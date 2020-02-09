341a5d02b9f44d40a7bd1ee18c36b0daf196dede
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-09 21:20:09 +0900

    Delete duplicated specs
    全く同じ内容のテストを２つ書いていたので片方を削除

--- spec/controllers/interviews_controller_spec.rb
+++ spec/controllers/interviews_controller_spec.rb
@@ -14,12 +14,6 @@ RSpec.describe InterviewsController, type: :controller do
         get :new, params: { user_id: @user.id }
         expect(response).to be_successful
       end
-
-      it 'returns a 200 response' do
-        sign_in @user
-        get :new, params: { user_id: @user.id }
-        expect(response).to have_http_status '200'
-      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'has a valid factory' do
-    expect(FactoryBot.build(:user)).to be_valid
-  end
-
   it 'is valid with email and password' do
     expect(FactoryBot.build(:user)).to be_valid
   end
