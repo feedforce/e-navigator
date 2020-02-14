86c6a2821696b85b814fdf32bb0135fce10ed852
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-14 12:27:16 +0900

    Set config.active_record.schema_format = :sql
    ENUM型のカラムを扱えるようにするためにconfigを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_141952) do
+ActiveRecord::Schema.define(version: 2020_02_05_150400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2020_02_12_141952) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role"
+    t.integer "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
