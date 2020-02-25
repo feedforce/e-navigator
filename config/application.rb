0972534419b7350c5a989558549e6f746ecd27d1
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-25 22:03:12 +0900

    Change schema format
    DB上のgenderとroleカラムの型をENUMから通常のストリングに変更するのに伴い修正

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module ENavigator
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.active_record.schema_format = :sql
+    config.active_record.schema_format = :ruby
   end
 end
