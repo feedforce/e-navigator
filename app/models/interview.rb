88c4c476dedc195f54649e5902e77f592297e2cb
punkshiraishi <pharmacy.sy@gmail.com>
2020-02-27 23:29:24 +0900

    Add validation to interview
    過去の日付をはじくバリデーションを追加

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -4,4 +4,9 @@ class Interview < ApplicationRecord
   belongs_to :user
   validates :interviewer_id, presence: true
   validates :schedule, presence: true
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    errors.add(:schedule, 'は過去の日付を選択できません。') if schedule.past?
+  end
 end
