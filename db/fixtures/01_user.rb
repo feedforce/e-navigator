deb871dceabef20d2db0134bb419036c78f5d86b
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-07 16:18:28 +0900

    Fix user and interview fixtures

--- db/fixtures/01_user.rb
+++ db/fixtures/01_user.rb
@@ -1,31 +1,47 @@
 # frozen_string_literal: true
 
-User.seed do |s|
-  s.id = 1
+interviewees = User.seed(:email) do |s|
   s.name = 'Yuma Shiraishi'
   s.email = 'shiraishi@test.com'
   s.password = 'password'
-  s.password_confirmation = 'password'
   s.gender = 'male'
   s.role = 'member'
 end
 
-User.seed do |s|
-  s.id = 2
+interviewers = User.seed(:email) do |s|
   s.name = 'Yosuke Obata'
   s.email = 'obata@test.com'
   s.password = 'password'
-  s.password_confirmation = 'password'
   s.gender = 'male'
   s.role = 'interviewer'
 end
 
-User.seed do |s|
-  s.id = 3
+Interview.seed do |s|
+  s.user_id = interviewees.first.id
+  s.interviewer_id = interviewers.first.id
+  s.schedule = '2020/01/01 14:00'
+  s.schedule_status = 'pending'
+end
+
+Interview.seed do |s|
+  s.user_id = interviewees.first.id
+  s.interviewer_id = interviewers.first.id
+  s.schedule = '2020/01/02 14:00'
+  s.schedule_status = 'fixed'
+end
+
+interviewers = User.seed(:email) do |s|
   s.name = 'Miki Miyazato'
   s.email = 'miyazato@test.com'
   s.password = 'password'
   s.password_confirmation = 'password'
   s.gender = 'female'
   s.role = 'interviewer'
+end
+
+Interview.seed do |s|
+  s.user_id = interviewees.first.id
+  s.interviewer_id = interviewers.first.id
+  s.schedule = '2020/01/03 14:00'
+  s.schedule_status = 'pending'
 end
--- db/fixtures/02_interview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Interview.seed do |s|
-  s.id = 1
-  s.user_id = 1
-  s.interviewer_id = 2
-  s.schedule = '2020/01/01 14:00'
-  s.is_fixed = false
-end
