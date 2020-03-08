8db1242d892ef157db8f521e1216751a944538c3
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-09 00:57:37 +0900

    Include integration helpers for request spec
    request spec 移行で controller 用のsign_in が使えなくなったことへの対応

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
