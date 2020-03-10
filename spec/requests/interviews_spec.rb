6d99898f4880a00795a1f85acd6846a248e9a57f
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-10 21:30:48 +0900

    Add interview request specs

--- spec/requests/interviews_spec.rb
+++ spec/requests/interviews_spec.rb
@@ -6,13 +6,45 @@ RSpec.describe InterviewsController, type: :request do
   describe 'GET #new' do
     before do
       @user = FactoryBot.create(:user)
+      @other_user = FactoryBot.create(:user)
     end
 
     context 'as an authenticated user' do
-      it 'responds successfully' do
+      it 'returns 200 response' do
         sign_in @user
         get new_user_interview_path(@user), params: { user_id: @user.id }
-        expect(response).to be_successful
+        expect(response).to have_http_status '200'
+      end
+    end
+
+    context 'as an unauthorized' do
+      it 'returns 302 response' do
+        sign_in @user
+        get new_user_interview_path(@other_user), params: { user_id: @other_user.id }
+        expect(response).to have_http_status '302'
+      end
+    end
+  end
+
+  describe 'GET #index' do
+    before do
+      @user = FactoryBot.create(:user)
+      @other_user = FactoryBot.create(:user)
+    end
+
+    context 'as an authenticated user' do
+      it 'returns 200 response' do
+        sign_in @user
+        get user_interviews_path(@user)
+        expect(response).to have_http_status '200'
+      end
+    end
+
+    context 'as an unauthorized' do
+      it 'returns 302 response' do
+        sign_in @user
+        get user_interviews_path(@other_user)
+        expect(response).to have_http_status '302'
       end
     end
   end
