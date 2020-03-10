54e1d71eae182fd8cff2f59f8096b68a02391a6c
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-11 00:41:14 +0900

    Fix user factories
    role を追加し、trait で member と interviewer を分けた

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@test.com" }
     password { 'password' }
+
+    trait :member do
+      role { 'member' }
+    end
+
+    trait :interviewer do
+      role { 'interviewer' }
+    end
   end
 end
