5d9ed77c7662eae5d890d2c56c316a3d4c2bef76
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-12 15:30:18 +0900

    Move interview validation error message to i18n
    他のバリデーションと同じようにするために ja.yml にエラーメッセージを移動

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -4,11 +4,12 @@ class Interview < ApplicationRecord
   belongs_to :user
   validates :interviewer_id, presence: true
   validates :schedule, presence: true
+  validates :schedule_status, presence: true
   validate :date_cannot_be_in_the_past
 
   enum schedule_status: { pending: 'pending', fixed: 'fixed' }
 
   def date_cannot_be_in_the_past
-    errors.add(:schedule, 'は過去の日付を選択できません。') if schedule.past?
+    errors.add(:schedule, I18n.t('errors.messages.past_date')) if schedule.present? && schedule.past?
   end
 end
