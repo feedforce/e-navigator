051860a978d395e34b84c45d154b91198cbb3d84
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-12 15:33:41 +0900

    Add interview model specs

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Interview, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with interviewer_id, schedule and schedule_status' do
+    expect(FactoryBot.build(:interview)).to be_valid
+  end
+
+  it 'is invalid without interviewer_id' do
+    interview = FactoryBot.build(:interview, interviewer_id: nil)
+    interview.valid?
+    expect(interview.errors[:interviewer_id]).to include(I18n.t('errors.messages.blank'))
+  end
+
+  it 'is invalid without schedule' do
+    interview = FactoryBot.build(:interview, schedule: nil)
+    interview.valid?
+    expect(interview.errors[:schedule]).to include(I18n.t('errors.messages.blank'))
+  end
+
+  it 'is invalid with schedule in the past' do
+    interview = FactoryBot.build(:interview, schedule: 1.minute.ago)
+    interview.valid?
+    expect(interview.errors[:schedule]).to include(I18n.t('errors.messages.past_date'))
+  end
 end
