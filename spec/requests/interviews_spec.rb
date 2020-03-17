d7f0aee1c7a60f3973495468c21066c9bf24b824
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-17 23:59:54 +0900

    Clean interview specs
    インスタンス変数の代わりに let を使うようにし、不要な部分を削除した

--- spec/requests/interviews_spec.rb
+++ spec/requests/interviews_spec.rb
@@ -3,47 +3,37 @@
 require 'rails_helper'
 
 RSpec.describe InterviewsController, type: :request do
-  describe 'GET #new' do
-    before do
-      @user = FactoryBot.create(:user)
-      @other_user = FactoryBot.create(:user)
-    end
+  let(:user) { FactoryBot.create(:user) }
 
+  describe 'GET #new' do
     context 'as an authenticated user' do
       it 'returns 200 response' do
-        sign_in @user
-        get new_user_interview_path(@user), params: { user_id: @user.id }
+        sign_in user
+        get new_user_interview_path(user), params: { user_id: user.id }
         expect(response).to have_http_status '200'
       end
     end
 
     context 'as an unauthorized' do
       it 'returns 302 response' do
-        sign_in @user
-        get new_user_interview_path(@other_user), params: { user_id: @other_user.id }
+        get new_user_interview_path(user), params: { user_id: user.id }
         expect(response).to have_http_status '302'
       end
     end
   end
 
   describe 'GET #index' do
-    before do
-      @user = FactoryBot.create(:user)
-      @other_user = FactoryBot.create(:user)
-    end
-
     context 'as an authenticated user' do
       it 'returns 200 response' do
-        sign_in @user
-        get user_interviews_path(@user)
+        sign_in user
+        get user_interviews_path(user)
         expect(response).to have_http_status '200'
       end
     end
 
     context 'as an unauthorized' do
       it 'returns 302 response' do
-        sign_in @user
-        get user_interviews_path(@other_user)
+        get user_interviews_path(user)
         expect(response).to have_http_status '302'
       end
     end
