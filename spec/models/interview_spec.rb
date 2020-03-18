c70c1d87c1810efa2a05f07b9585e3910651437a
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-18 12:30:09 +0900

    Clean interview model specs
    エラーメッセージの直接の参照をしないように変更
    インスタンス変数ではなくlet を使うように変更

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -3,25 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe Interview, type: :model do
+  let(:member) { FactoryBot.create(:user, :member) }
+  let(:interviewer) { FactoryBot.create(:user, :interviewer) }
+
   it 'is valid with interviewer_id, schedule and schedule_status' do
-    expect(FactoryBot.build(:interview)).to be_valid
+    interview = Interview.new(user_id: member.id, interviewer_id: interviewer.id, schedule: Time.now.tomorrow)
+    expect(interview.valid?).to be_truthy
   end
 
   it 'is invalid without interviewer_id' do
-    interview = FactoryBot.build(:interview, interviewer_id: nil)
+    interview = Interview.new(user_id: member.id, interviewer_id: nil, schedule: Time.now.tomorrow)
     interview.valid?
-    expect(interview.errors[:interviewer_id]).to include(I18n.t('errors.messages.blank'))
+    expect(interview.errors.of_kind?(:interviewer_id, :blank)).to be_truthy
   end
 
   it 'is invalid without schedule' do
-    interview = FactoryBot.build(:interview, schedule: nil)
+    interview = Interview.new(user_id: member.id, interviewer_id: interviewer.id, schedule: nil)
     interview.valid?
-    expect(interview.errors[:schedule]).to include(I18n.t('errors.messages.blank'))
+    expect(interview.errors.of_kind?(:schedule, :blank)).to be_truthy
   end
 
   it 'is invalid with schedule in the past' do
-    interview = FactoryBot.build(:interview, schedule: 1.minute.ago)
+    interview = Interview.new(user_id: member.id, interviewer_id: interviewer.id, schedule: 1.minute.ago)
     interview.valid?
-    expect(interview.errors[:schedule]).to include(I18n.t('errors.messages.past_date'))
+    expect(interview.errors.of_kind?(:schedule, :past_date)).to be_truthy
   end
 end
