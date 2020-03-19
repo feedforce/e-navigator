74dfb269b87039603171955e430861943b11852b
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-19 22:24:53 +0900

    Fix interview model specs to use build method

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -7,24 +7,24 @@ RSpec.describe Interview, type: :model do
   let(:interviewer) { FactoryBot.create(:user, :interviewer) }
 
   it 'is valid with interviewer_id, schedule and schedule_status' do
-    interview = Interview.new(user_id: member.id, interviewer_id: interviewer.id, schedule: Time.now.tomorrow)
+    interview = member.interviews.build(interviewer_id: interviewer.id, schedule: Time.now.tomorrow)
     expect(interview.valid?).to be_truthy
   end
 
   it 'is invalid without interviewer_id' do
-    interview = Interview.new(user_id: member.id, interviewer_id: nil, schedule: Time.now.tomorrow)
+    interview = member.interviews.build(interviewer_id: nil, schedule: Time.now.tomorrow)
     interview.valid?
     expect(interview.errors.of_kind?(:interviewer_id, :blank)).to be_truthy
   end
 
   it 'is invalid without schedule' do
-    interview = Interview.new(user_id: member.id, interviewer_id: interviewer.id, schedule: nil)
+    interview = member.interviews.build(interviewer_id: interviewer.id, schedule: nil)
     interview.valid?
     expect(interview.errors.of_kind?(:schedule, :blank)).to be_truthy
   end
 
   it 'is invalid with schedule in the past' do
-    interview = Interview.new(user_id: member.id, interviewer_id: interviewer.id, schedule: 1.minute.ago)
+    interview = member.interviews.build(interviewer_id: interviewer.id, schedule: 1.minute.ago)
     interview.valid?
     expect(interview.errors.of_kind?(:schedule, :past_date)).to be_truthy
   end
