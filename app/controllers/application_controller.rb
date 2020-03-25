e68533572c723c9b3bff87ce047616094d5bd0f5
punkshiraishi <pharmacy.sy@gmail.com>
2020-03-25 21:52:38 +0900

    Delete current_user? method.
    このメソッドを呼出していたメソッドを削除したのに伴い削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def current_user?(user)
-    user == current_user
-  end
-
   protected
 
   def configure_permitted_parameters
